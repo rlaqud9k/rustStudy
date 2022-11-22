707729f80885474380f93e7d1cd9fc73888cf590
byeonggwan-kim <byeonggwan_kim@linc-well.com>
2022-11-22 18:34:26 +0900

    carTest

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,65 @@
-fn main() {
-    // We have orders for three new cars!
-    // We'll declare a mutable car variable and reuse it for all the cars
-    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
-    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
-
-    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
-    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
-
-    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
-    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
-    println!("{:?}", car.transmission); 
-}
-
-// Declare Car struct to describe vehicle with four named fields
+#[derive(PartialEq, Debug)]
 struct Car {
     color: String,
-    transmission: Transmission,
-    convertible: bool,
-    mileage: u32,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32)
 }
-
 #[derive(PartialEq, Debug)]
-// Declare enum for Car transmission type
-enum Transmission {
-    // todo!("Fix enum definition so code compiles");
-    Manual,
-    SemiAuto,
-    Automatic
+enum Transmission { Manual, SemiAuto, Automatic }
+#[derive(PartialEq, Debug)]
+enum Age { New, Used }
+
+fn car_quality (miles: u32) -> (Age, u32) {
+    let quality: (Age, u32) = (Age::New, miles);
+    return quality
 }
 
-fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
 
-    // Use the values of the input arguments
-    // All new cars always have zero mileage
     Car {
         color: color,
-        transmission: transmission,
-        convertible: convertible,
-        mileage: 0
+        motor: motor,
+        roof: roof,
+        age: car_quality(miles)
     }
 }
 
+//////////////////////////////////////////////////
+
+fn main() {
+
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    let mut car: Car;
+    let mut engine = Transmission::Manual;
+
+    car = car_factory(String::from(colors[0]), engine, true, 0);
+    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+    
+
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+}
+
+fn arrayTest () {
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+
+    let three_nums = vec![15, 3, 24];
+    let three = three_nums[1];
+    println!("Vector: {:?}, three = {}", three_nums, three); 
+    let zeroes = vec![0; 5];
+    println!("Zeroes: {:?}", zeroes); 
+
+    let mut fruit = Vec::<&str>::new();
+    fruit.push("Apple");
+    fruit.push("Apple");
+    fruit.push("Apple");
+    println!("Fruits: {:?}", fruit[3]); 
+}
